fdba53e1671953ae23220286c5bdcdb6599e5c7f
AlexandrosMo <alexandros.moraitis99@gmail.com>
2025-06-28 21:47:27 +0300

    Dockerfile

--- server-stats/server-stats.sh
+++ server-stats/server-stats.sh
@@ -1,33 +1,37 @@
-echo "###############################################"
-echo "# System Uptime Info #"
+#!/bin/bash
 
-Uptime
+echo "###############################################"
+echo "# System uptime Info #"
+echo "###############################################"
+
+uptime
 
 echo
-
 echo "###############################################"
 echo "# Total CPU Usage #"
 echo "###############################################"
-
-top -bn1 | grep "%Cpu(s)" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
+top -bn1 | grep "%Cpu(s)" | awk '{print "Usage: " 100 - $8 "%"}'
 
 echo
-
 echo "###############################################"
 echo "# Memory Usage #"
 echo "###############################################"
-free | grep "Mem:" -w | awk '{print "Total: %.1fGi\nUsed: %.1fGi\n (%.2f%%)\nFree: %.1fGi (%.2f%%)\n",$2/1024^2, $3/1024^2, $3/$2 * 100, $4/1024^2,$4/$2 * 100}`
+free | grep "Mem:" | awk '{ 
+  total=$2/1024/1024; 
+  used=$3/1024/1024; 
+  free=$4/1024/1024;
+  printf "Total: %.1f GiB\nUsed: %.1f GiB (%.2f%%)\nFree: %.1f GiB (%.2f%%)\n", 
+    total, used, (used/total)*100, free, (free/total)*100
+}'
 
 echo
-
 echo "###############################################"
 echo "# Top 5 Processes by Memory Usage #"
 echo "###############################################"
-ps aux --sort -%mem | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}`
+ps aux --sort=-%mem | awk 'NR==1 || NR<=6 {print $1 "\t" $2 "\t" $4 "\t" $11}'
 
 echo
-
 echo "###############################################"
 echo "# Top 5 Processes by CPU Usage #"
 echo "###############################################"
-ps aux --sort -%cpu | head -n 6 | awk `{print $1 "\t" $2 "\t" $3 "\t" $11}`
+ps aux --sort=-%cpu | awk 'NR==1 || NR<=6 {print $1 "\t" $2 "\t" $3 "\t" $11}'
