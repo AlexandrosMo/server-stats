8b565f23debe692ab8ec017bb903b386ed38704e
AlexandrosMo <alexandros.moraitis99@gmail.com>
2025-07-15 14:26:13 +0300

    changes

---
+++ server-stats/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Φορτώνει το .env
+
+app = Flask(__name__)
+
+PORT = int(os.getenv("PORT", 8081))  # default port 8081
+
+@app.route('/')
+def home():
+    return "Hello from server-stats API!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
